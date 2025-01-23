fc582edd97306449a02a9967cac87a0ca2b47733
khudiiash <d.khudiiash@bmlabs.org>
2025-01-23 15:06:53 +0200

    fix sponza demo

--- demos/intermediate/Sponza/Sponza.ts
+++ demos/intermediate/Sponza/Sponza.ts
@@ -2,7 +2,7 @@ import { Engine } from '@/engine/Engine';
 import { SponzaScene } from './scenes/SponzaScene';
 
 async function main() {
-    const engine = await Engine.getInstance().init();
+    const engine = await Engine.getInstance({ fullscreen: true }).init();
     const demo = new SponzaScene(engine);
     await demo.init();
     demo.start();
